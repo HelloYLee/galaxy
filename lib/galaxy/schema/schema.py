1b408a6f82fdb24ddd02ff97f7302fd6974aaaa5
mvdbeek <m.vandenbeek@gmail.com>
2024-10-16 14:50:08 +0200

    Separate private and public landing requests
    Private requests must be claimed before use. Public / Anonymous requests
    only depend on the public flag, so anoymous users can create private
    landing requests and authenticated users can create public requests.
    
    Tests should cover all of these scenarios.

--- lib/galaxy/schema/schema.py
+++ lib/galaxy/schema/schema.py
@@ -3867,6 +3867,7 @@ class CreateToolLandingRequestPayload(Model):
     tool_version: Optional[str] = None
     request_state: Optional[Dict[str, Any]] = None
     client_secret: Optional[str] = None
+    public: bool = False
 
 
 class CreateWorkflowLandingRequestPayload(Model):
@@ -3874,6 +3875,10 @@ class CreateWorkflowLandingRequestPayload(Model):
     workflow_target_type: Literal["stored_workflow", "workflow", "trs_url"]
     request_state: Optional[Dict[str, Any]] = None
     client_secret: Optional[str] = None
+    public: bool = Field(
+        False,
+        description="If workflow landing request is public anyone with the uuid can use the landing request. If not public the request must be claimed before use and additional verification might occur.",
+    )
 
 
 class ClaimLandingPayload(Model):
